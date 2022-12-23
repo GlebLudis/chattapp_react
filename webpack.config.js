b5ded3c282e057b148ed21a9c60cd795ef4b2936
Gleb Ludis <glebludis@yandex.ru>
2022-12-23 17:26:09 +0300

    add template

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx'],
+  },
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      title: 'React',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/[hash][ext]',
+        },
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    open: true,
+  },
+};
