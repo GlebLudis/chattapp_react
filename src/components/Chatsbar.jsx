9431d4bf28f1424d69cde00990fa191b928f4a8f
Gleb Ludis <glebludis@yandex.ru>
2022-12-24 13:20:40 +0300

    add components for homepage

---
+++ src/components/Chatsbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChatBar () {
+    return(
+        <></>
+    )
+}
+
+export default ChatBar
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Homepage () {
     return (
-        <></>
+        <div className="homepage">
+            <div className="container"></div>
+        </div>
     )
 }
 export default Homepage
